d34770016d75de4ba343be369ffd1888b31e29b4
AKrasts <krastsarturs49@gmail.com>
2024-05-03 21:09:57 +0300

    port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import sys
 
 app = Flask(__name__)
 
@@ -7,4 +8,7 @@ def greetings():
     return jsonify({"greeting": "Hello from Python App!"})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, port="3000")
+    port = 3000
+    if sys.argv[2].isnumeric(): 
+        port = sys.argv[2]
+    app.run(host="0.0.0.0", debug=True, port=port)
